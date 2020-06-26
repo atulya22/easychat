780ffc01c0ea5719020e66286a9de074edca10ba
atulya22 <ashetty@uab.edu>
2020-06-25 21:00:45 -0400

    Update target os and orientation support

--- EasyChat/Controllers/DiscussionsViewController.swift
+++ EasyChat/Controllers/DiscussionsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  DiscussionsViewController.swift
 //  EasyChat
 //
 //  Created by Atulya Shetty on 6/25/20.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class DiscussionsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
