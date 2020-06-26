5b372063bd1e30e615ee9aa9da63f374accafcfc
atulya22 <ashetty@uab.edu>
2020-06-25 20:38:44 -0400

    Initial project structure

---
+++ EasyChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EasyChat
+//
+//  Created by Atulya Shetty on 6/25/20.
+//  Copyright © 2020 Atulya Shetty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
